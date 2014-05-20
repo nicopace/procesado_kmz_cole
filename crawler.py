144ba5583dd2f6ffb83262d90d2e2869df6acbae
Nicolás Pace <nicopace@gmail.com>
2014-05-20 15:29:45 -0300

    added path data

--- crawler.py
+++ crawler.py
@@ -2,8 +2,29 @@ from pcontrol_reader import pcontrol_read
 from pas_reader import pas_read
 from rec_reader import rec_read
 from stop_reader import stop_read
+from datetime import datetime, tzinfo
+from pytz import timezone
 
 import os
+
+companies = {
+    '1': 'Fournier',
+    '2': 'Plaza',
+    '4': 'Villarino',
+    '7': 'Sapem',
+    '8': 'San Gabriel'
+}
+
+local_tz = timezone('America/Argentina/Buenos_Aires')
+
+def extract_date(path_data):
+    for i, value in enumerate(path_data):
+        try:
+            path_data[i] = int(value)
+        except:
+            pass
+
+    return datetime(path_data[2], path_data[3], path_data[4], path_data[5], tzinfo=local_tz)
 
 def process(root, name):
     filename = os.path.join(root, name)
@@ -20,8 +41,16 @@ def process(root, name):
     elif 'pcontrol' in name:
         result = pcontrol_read(filename)
         result['type'] = 'pcontrol'
+    else:
+        result = []
 
     result['filename'] = filename
+    path_data  = filename.split('/')
+    company_id = path_data[1]
+    data_date = extract_date(path_data)
+
+
+    result['company'] = companies[company_id]
 
     return result
 
